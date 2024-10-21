959cd0436319b404585fb11aff04021fa5a3b3a6
LucaM0001 <lucamamitiana@gmail.com>
2024-10-21 12:47:34 +0000

    Creation des composants Header, Landing et Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé par LucaM0001 - 2024</p>
+        <p>
+          Les icônes Wolverine, Iron-man, Spider-man, batman sont prises sur
+          iconFinder.com
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
