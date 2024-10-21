7a5e1cfca1a01764119d3f31f72141b4c5eeba89
LucaM0001 <lucamamitiana@gmail.com>
2024-10-21 14:49:32 +0000

    Instanciation de la class Firebase via le Contexte

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import Firebase, { FirebaseContext } from "./Firebase"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </StrictMode>
 )
