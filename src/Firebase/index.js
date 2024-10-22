40dc4db1d67e3caf94182ee9d16829ac6b24f426
LucaM0001 <lucamamitiana@gmail.com>
2024-10-22 09:32:59 +0000

    Gestion de la session utilisateur via Firebase

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,5 +1,6 @@
+import { onAuthStateChanged } from "firebase/auth"
 import FirebaseContext from "./context"
 import Firebase from "./firebase"
 
 export default Firebase
-export { FirebaseContext }
+export { FirebaseContext, onAuthStateChanged }
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,32 @@
+import { useContext, useEffect, useState } from "react"
 import Logout from "../Logout/Logout"
 import Quiz from "../Quiz/Quiz"
+import { FirebaseContext, onAuthStateChanged } from "../../Firebase"
+import { useNavigate } from "react-router-dom"
 
 const Welcome = () => {
-  return (
+  const navigate = useNavigate()
+
+  const firebase = useContext(FirebaseContext)
+
+  const [userSession, setUserSession] = useState(null)
+
+  useEffect(() => {
+    let listener = onAuthStateChanged(firebase.auth, (user) => {
+      user ? setUserSession(user) : navigate("/")
+    })
+
+    return () => {
+      listener()
+    }
+  }, [])
+
+  return userSession === null ? (
+    <>
+      <div className="loader"></div>
+      <p>Loading...</p>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
