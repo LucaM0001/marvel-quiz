41632d3fe00b1c502f1152deffda778a81ab6bbe
LucaM0001 <lucamamitiana@gmail.com>
2024-10-22 15:28:54 +0000

    Composant Quiz, Levels et Progressbar

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react"
+import Levels from "../Levels/Levels"
+import ProgressBar from "../ProgressBar/ProgressBar"
 
 class Quiz extends Component {
   render() {
     const { pseudo } = this.props.userData
     return (
       <div>
-        <h2>Pseudo: {pseudo}</h2>
+        <Levels />
+        <ProgressBar />
+        <h2>Notre question Quiz</h2>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 2</p>
+        <p className="answerOptions">Question 3</p>
+        <p className="answerOptions">Question 4</p>
+        <button className="btnSubmit">Suivant</button>
       </div>
     )
   }
