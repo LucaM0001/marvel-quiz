b858de76fa767f91e4a6e7a488c6e5d242b5b4a8
LucaM0001 <lucamamitiana@gmail.com>
2024-11-08 10:47:37 +0000

    Utilisation du package react-form-stepper

--- src/components/Levels/Levels.jsx
+++ src/components/Levels/Levels.jsx
@@ -1,11 +1,23 @@
-import React from "react"
+import { memo, useEffect, useState } from "react"
+import { Stepper } from "react-form-stepper"
 
-const Levels = () => {
+const Levels = ({ levelNames, quizLevel }) => {
+  const [levels, setLevels] = useState([])
+
+  useEffect(() => {
+    const quizSteps = levelNames.map((level) => ({
+      label: level.toUpperCase(),
+    }))
+    setLevels(quizSteps)
+  }, [levelNames])
+
   return (
-    <div className="levelsContainer">
-      <h2 className="headingLevels">Débutant</h2>
-    </div>
+    <Stepper
+      steps={levels}
+      activeStep={quizLevel}
+      styleConfig={{ size: 45, circleFontSize: 20 }}
+    />
   )
 }
 
-export default Levels
+export default memo(Levels)
